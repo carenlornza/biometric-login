464138cc6b4fa8c0a109b28092ea09e10a37259a
carenlornza <82206244+carenlornza@users.noreply.github.com>
2025-07-01 20:50:14 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$db   = 'uas';
-$user = 'root';
-$pass = '';
+$host = 'sql12.freesqldatabase.com';
+$db   = 'sql12787748';
+$user = 'sql12787748';
+$pass = 'uICt1bSbu9';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
